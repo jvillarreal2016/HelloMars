7259f09eeeaad1ec51032137d77abf88c9047690
Javier <jvillarreal2016@gmail.com>
2021-01-28 00:50:31 -0600

    first commit

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Start()
+    {
+        StartScene();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NextScene();
+        }
+    }
+
+    public void NextScene() {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+
+    public void StartScene() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public static void QuitGameEarly() {
+        Application.Quit();
+    }
+}
